3465cca6fa26736dc4b99f05b77cb5a8d0587796
Brad Morris <brad@bradleymorris.co.uk>
2013-05-14 22:01:13 +0100

    Undone mistake of removing config. Duh...

---
+++ config.js
@@ -0,0 +1,11 @@
+config = {}
+config.cloudflare = {}
+config.cloudflare.email = "your cloudflare login email"
+config.cloudflare.tkn = "secretstuff"
+config.cloudflare.zone = "eg.. mydomain.co.uk"
+
+config.endpoints = {}
+config.endpoints.ip = "http://bot.whatismyipaddress.com"
+config.endpoints.cloudflare = "https://www.cloudflare.com/api_json.html"
+
+module.exports = config
